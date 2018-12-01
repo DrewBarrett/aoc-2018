c5db4161c7ec548ab97441cd762780f885bf3577
Drew Barrett <dfb1998@gmail.com>
2018-12-01 14:02:21 -0500

    d1p2

--- day1/main.py
+++ day1/main.py
@@ -3,6 +3,18 @@
 if __name__ == "__main__":
     f = open("input", 'r')
     s = 0
-    for l in f:
-        s += int(l)
-    print(s)
+    ff = [0]
+    re = 0
+    p2 = True
+    f = f.readlines()
+    while p2:
+        for l in f:
+            s += int(l)
+            if p2 == True:
+                if (s in ff):
+                    re = s
+                    p2 = False
+                ff.append(s)
+        print("Part 1:", s)
+
+    print("Part 2:", re)
