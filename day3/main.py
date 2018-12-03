ebbe6d011ca22c0d6a47c25eb665ccb11e3b1d27
Drew Barrett <dfb1998@gmail.com>
2018-12-02 23:44:48 -0500

    prep for day3

---
+++ day3/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def p1(lines):
+    return 0
+def p2(lines):
+    return 0
+
+def main():
+    f = open('input').readlines()
+    return 0
+
+if __name__ == "__main__":
+    main()
