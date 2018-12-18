88290e348aa7fa07535aa54d967756483d6d1c53
Drew Barrett <dfb1998@gmail.com>
2018-12-17 23:52:33 -0500

    day 18 init

---
+++ day18/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def p1(lines):
+    return 0
+def p2(lines):
+    return 0
+
+def main():
+    f = open('input').readlines()
+    p1(f)
+    p2(f)
+    return 0
+
+if __name__ == "__main__":
+    main()
