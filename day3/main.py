028bf86efe9612b6a55a2186000a33d0c9ae4891
Drew Barrett <dfb1998@gmail.com>
2018-12-21 21:53:02 -0500

    day3 p2

--- day3/main.py
+++ day3/main.py
@@ -2,38 +2,59 @@
 
 def p1(lines):
     c = 0
-    board = {} # TODO: Add ownership table
+    board = {}
+    claims = []
     for line in lines:
         line = line.split(' ')
+        claim = line[0][1:]
         offsets = line[2][:-1].split(',')
         size = line[3].split('x')
+        claims.append(claim)
         for i in range(int(size[0])):
             for j in range(int(size[1])):
                 if board.get(i + int(offsets[0])) == None:
                     board[i + int(offsets[0])] = {}
-                    board[i + int(offsets[0])][j + int(offsets[1])] = 1
+                    board[i + int(offsets[0])][j + int(offsets[1])] = {'claims': 1, 'owner': claim}
                 else:
                     if board.get(i + int(offsets[0])).get(j + int(offsets[1])) == None:
-                        board[i + int(offsets[0])][j + int(offsets[1])] = 1
+                        board[i + int(offsets[0])][j + int(offsets[1])] = {'claims': 1, 'owner': claim}
                     else:
-                        board[i + int(offsets[0])][j + int(offsets[1])] += 1
+                        board[i + int(offsets[0])][j + int(offsets[1])]['claims'] += 1
+                        #if board[i + int(offsets[0])][j + int(offsets[1])]['owner'] is not claim:
+                        try:
+                            #print('removing', (board[i + int(offsets[0])][j + int(offsets[1])]['owner']))
+                            claims.remove(board[i + int(offsets[0])][j + int(offsets[1])]['owner'])
+                        except:
+                            #print('failed')
+                            pass
+                        try:
+                            claims.remove(claim)
+                        except:
+                            pass
+                        board[i + int(offsets[0])][j + int(offsets[1])]['owner'] = claim
+        #print(board, claims)
     for i in board.keys():
+        string = ""
         for j in board[i].keys():
-            if board[i][j] >= 2:
+            if board[i][j]['claims'] >= 2:
+                string += 'X'
                 c += 1
-            if board[i][j] == 0:
+            elif board[i][j]['claims'] == 0:
                 print("")
-    print(c)
-    return c
+            elif board[i][j]['claims'] == 1:
+                string += board[i][j]['owner']
+        #print(string)
+    #print(claims)
+    return c, int(claims[0])
 def p2(lines):
     return 0
 
 def main():
     assert p1(['#1 @ 1,3: 4x4',
                '#2 @ 3,1: 4x4',
-               '#3 @ 5,5: 2x2']) == 4
+               '#3 @ 5,5: 2x2']) == (4, 3)
     f = open('input').readlines()
-    p1(f)
+    print(p1(f))
     p2(f)
     return 0
 
